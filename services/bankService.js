7b5e585598eaa5ee545c44868e7ef79519463855
Stedman <Steddyrockin@gmail.com>
2026-03-13 15:03:30 -0700

    bankService.jsv

---
+++ services/bankService.js
@@ -0,0 +1,9 @@
+async function sendToBank(amount) {
+
+  // Integrate Stripe/Dwolla/Plaid here
+  console.log(`Transferring $${amount} to bank account`);
+
+  return { success: true };
+}
+
+module.exports = { sendToBank };
