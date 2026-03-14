87e532583483e49b1221889ec66755cb7bc4c9dc
Stedman <Steddyrockin@gmail.com>
2026-03-14 16:19:01 -0700

    Update app.py

--- services/ai/app.py
+++ services/ai/app.py
@@ -1,7 +1,18 @@
+# ai-engine/app.py
+
 from fastapi import FastAPI
+from models.lstm import predict_price
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"status": "AI Engine Running"}
+@app.post("/predict")
+def predict(data: dict):
+
+    current = data["current_price"]
+
+    predicted = predict_price(data)
+
+    return {
+        "current": current,
+        "predicted": predicted
+    }
