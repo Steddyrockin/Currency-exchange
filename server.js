6720813e88f63bdba6339a77afce5c814b9b818b
Stedman <Steddyrockin@gmail.com>
2026-03-13 18:49:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const express = require("express");
-const http = require("http");
-const WebSocket = require("ws");
-require("dotenv").config();
-
 const app = express();
+
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/currency", require("./routes/currencyRoutes"));
-app.use("/api/trade", require("./routes/tradeRoutes"));
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const PORT = 5000;
 
-wss.on("connection", (ws) => {
-  ws.send(JSON.stringify({ status: "Connected to Market Stream" }));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-server.listen(5000, () =>
-  console.log("Currency Exchange running on port 5000")
-);
