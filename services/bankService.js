647f6b1e96b633d51a204a27d5e33229bcef4d50
Stedman <Steddyrockin@gmail.com>
2026-03-14 00:07:33 -0700

    Update bankService.js

--- services/bankService.js
+++ services/bankService.js
@@ -1,11 +1,11 @@
-// services/bankService.js
-
 async function sendToBank(amount) {
 
-  // Integrate Stripe/Dwolla/Plaid here
-  console.log(`Transferring $${amount} to bank account`);
+  console.log(`Sending $${amount} payout to Chase`);
 
-  return { success: true };
+  // connect ACH provider here
+  // Stripe Treasury / Dwolla / Plaid
+
+  return true;
 }
 
 module.exports = { sendToBank };
