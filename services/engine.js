fd7e65ad750acefb748ec2d63f33f2e23423b8a1
Stedman <Steddyrockin@gmail.com>
2026-03-15 16:18:31 -0700

    Create engine.js

---
+++ services/engine.js
@@ -0,0 +1,12 @@
+async function runAI(){
+
+// Connect to model
+// Analyze volatility
+// Generate signals
+// Update portfolio allocations
+
+console.log("AI Engine Executed")
+
+}
+
+module.exports = { runAI }
